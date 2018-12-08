0956e8700fa47f66b95b6c9d2e5bc07f23d58679
LZH8080 <40724443@qq.com>
2018-12-08 10:32:45 +0800

    java test commit02

---
+++ MyPro01/src/com/zeasn/start/UserStart.java
@@ -0,0 +1,19 @@
+package com.zeasn.start;
+
+import com.zeasn.UserService;
+
+public class UserStart implements UserService{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		UserStart uStart = new UserStart();
+		uStart.start();
+	}
+
+	@Override
+	public int start() {
+		System.out.println("UserStart...");
+		return 0;
+	}
+
+}
